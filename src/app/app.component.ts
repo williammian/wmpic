2e318382999f24f7360f19ea2eaa1833ecdf4ec5
William Mian <william_mian@yahoo.com.br>
2022-02-03 13:53:45 -0300

    criando componente e inbound properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wmpic';
-  description = 'Leão';
-  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/239px-Sultan_the_Barbary_Lion.jpg';
+
 }
