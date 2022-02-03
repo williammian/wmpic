a0bd1aa0d0e3504c430c5d0840987f8282340839
William Mian <william_mian@yahoo.com.br>
2022-02-03 09:57:55 -0300

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wmpic';
+  description = 'Leão';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/239px-Sultan_the_Barbary_Lion.jpg';
 }
