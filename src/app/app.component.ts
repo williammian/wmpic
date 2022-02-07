382d3769ef7b9c4e80829feae99f6846b184036b
William Mian <william_mian@yahoo.com.br>
2022-02-07 10:31:14 -0300

    consumindo dados da API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photos = [];
+  photos: Object[] = [];
 
   constructor(http: HttpClient) {
-    console.log(http);
+
+    http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe(photos => this.photos = photos);
+
   }
 
 }
