18f6e3e0ee9744d711cb6f07ada35b11a0883a46
William Mian <william_mian@yahoo.com.br>
2022-02-08 13:12:21 -0300

    segmento de rotas

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from './../photo/photo.service';
 
 @Component({
@@ -10,11 +11,15 @@ export class PhotoListComponent implements OnInit {
 
   photos: any[] = [];
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService
-        .listFromUser('flavio')
+        .listFromUser(userName)
         .subscribe(photos => this.photos = photos);
   }
 
