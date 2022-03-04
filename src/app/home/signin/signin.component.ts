0febe3d9de4655057cc7ebd5bd7633d4d3151bb6
William Mian <william_mian@yahoo.com.br>
2022-03-04 10:33:02 -0300

    detectando a plataforma de execucao

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
+import { PlatformDetectorService } from './../../core/platform-detector/platform-detector.service';
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-import { AuthService } from "src/app/core/auth.service";
+import { AuthService } from "src/app/core/auth/auth.service";
 
 @Component({
   templateUrl: './signin.component.html'
@@ -14,7 +15,8 @@ export class SignInComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private platformDetectorService: PlatformDetectorService
     ) { }
 
   ngOnInit(): void {
@@ -35,7 +37,10 @@ export class SignInComponent implements OnInit {
           err => {
             console.log(err);
             this.loginForm.reset();
-            this.userNameInput.nativeElement.focus();
+
+            this.platformDetectorService.isPlatformBrowser() &&
+              this.userNameInput.nativeElement.focus();
+
             alert('Invalid user name or password');
           }
         );
