4da22f12ef76f6b110224b8867051f7561d3383a
William Mian <william_mian@yahoo.com.br>
2022-03-24 14:53:52 -0300

    submissao dos dados signup

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { SignUpService } from './signup.service';
+import { NewUser } from './new-user';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -12,7 +15,9 @@ export class SignUpComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private userNotTakenValidatorService: UserNotTakenValidatorService) {  }
+    private userNotTakenValidatorService: UserNotTakenValidatorService,
+    private signUpService: SignUpService,
+    private router: Router) {  }
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
@@ -46,6 +51,16 @@ export class SignUpComponent implements OnInit {
                 ]
             ]
     });
+  }
+
+  signup() {
+    const newUser = this.signupForm.getRawValue() as NewUser;
+    this.signUpService
+        .signup(newUser)
+        .subscribe(
+            () => this.router.navigate(['']),
+            err => console.log(err)
+        );
   }
 
 }
--- src/app/home/signup/signup.service.ts
+++ src/app/home/signup/signup.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NewUser } from './new-user';
 
 const API_URL = "http://localhost:3000";
 
@@ -12,4 +13,9 @@ export class SignUpService {
 
         return this.http.get(API_URL + '/user/exists/' + userName);
     }
+
+    signup(newUser: NewUser) {
+      return this.http.post(API_URL + '/user/signup', newUser);
+    }
+
 }
