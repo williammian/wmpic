9c18545448f8f5ea070853f54093752a460c30e8
William Mian <william_mian@yahoo.com.br>
2022-03-28 10:28:55 -0300

    interceptadores e envio do token

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
   declarations: [ HeaderComponent ],
@@ -9,7 +11,14 @@ import { HeaderComponent } from "./header/header.component";
   imports: [
     CommonModule ,
     RouterModule
-  ]
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
+]
 })
 export class CoreModule {
 
