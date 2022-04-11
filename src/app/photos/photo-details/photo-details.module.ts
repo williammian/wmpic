79e0a772feb4437a2754ce5789507afe23d78d7a
William Mian <william_mian@yahoo.com.br>
2022-04-11 15:07:11 -0300

    ocultando elementos que requerem permissao

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoDetailsComponent } from "./photo-details.component";
 import { PhotoCommentsComponent } from "./photo-commnets/photo-commnets.component";
+import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.directive';
 import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 
 @NgModule({
     declarations: [
       PhotoDetailsComponent,
-      PhotoCommentsComponent
+      PhotoCommentsComponent,
+      PhotoOwnerOnlyDirective
     ],
     exports: [
       PhotoDetailsComponent,
---
+++ src/app/photos/photo-details/photo-owner-only/photo-owner-only.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Photo } from "../../photo/photo";
+import { Input } from "@angular/core";
+import { UserService } from "src/app/core/user/user.service";
+
+@Directive({
+    selector :'[photoOwnerOnly]'
+})
+export class PhotoOwnerOnlyDirective implements OnInit {
+
+  @Input() ownedPhoto: Photo;
+
+  constructor(
+    private element: ElementRef<any>,
+    private renderer: Renderer2,
+    private UserService: UserService
+  ) {}
+
+  ngOnInit() : void {
+    this.UserService
+        .getUser()
+        .subscribe(user => {
+          if(!user || user.id != this.ownedPhoto.userId) {
+            this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
+          }
+        });
+  }
+
+}
