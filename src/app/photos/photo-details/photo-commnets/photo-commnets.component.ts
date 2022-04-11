197f5ec88aa92b684fbfa95984e9daa99211f668
William Mian <william_mian@yahoo.com.br>
2022-04-11 13:12:18 -0300

    validacao

--- src/app/photos/photo-details/photo-commnets/photo-commnets.component.ts
+++ src/app/photos/photo-details/photo-commnets/photo-commnets.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Input } from "@angular/core";
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 import { Observable } from "rxjs";
 import { PhotoComment } from "../../photo/photo-comment";
 import { PhotoService } from "../../photo/photo.service";
@@ -11,12 +13,19 @@ import { PhotoService } from "../../photo/photo.service";
 export class PhotoCommentsComponent implements OnInit {
 
     @Input() photoId: number;
+    commnetForm: FormGroup;
 
     comments$: Observable<PhotoComment[]>;
 
-    constructor(private photoService: PhotoService) {}
+    constructor(
+      private photoService: PhotoService,
+      private formBuilder: FormBuilder
+    ) {}
 
     ngOnInit(): void {
         this.comments$ = this.photoService.getComments(this.photoId);
+        this.commnetForm = this.formBuilder.group({
+          comment: ['', Validators.maxLength(300)]
+        });
     }
 }
--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoDetailsComponent } from "./photo-details.component";
 import { PhotoCommentsComponent } from "./photo-commnets/photo-commnets.component";
+import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 
 @NgModule({
     declarations: [
@@ -17,7 +19,9 @@ import { PhotoCommentsComponent } from "./photo-commnets/photo-commnets.componen
     imports: [
       CommonModule,
       PhotoModule,
-      RouterModule
+      RouterModule,
+      ReactiveFormsModule,
+      VMessageModule
     ]
 })
 export class PhotoDetailsModule { }
