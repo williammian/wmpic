ca9431685d9b975c2b5514f9a1ead1d422b2240d
William Mian <william_mian@yahoo.com.br>
2022-04-14 09:37:34 -0300

    componente indicador de trabalho processando

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { LoadingService } from "./loading.service";
+import { Observable } from "rxjs";
+import { LoadingType } from "./loading-type";
+import { map } from "rxjs/operators";
+
+@Component({
+    selector: 'ap-loading',
+    templateUrl: './loading.component.html'
+})
+export class LoadingComponent implements OnInit {
+
+    loading$: Observable<string>
+
+    constructor(private loadingService: LoadingService) {}
+
+    ngOnInit(): void {
+      this.loading$ = this.loadingService
+          .getLoading()
+          .pipe(map(loadingType => loadingType.valueOf()));
+    }
+}
