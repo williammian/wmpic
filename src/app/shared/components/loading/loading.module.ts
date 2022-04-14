2f5da48995ad457424b54534f3f2237c5dcab39f
William Mian <william_mian@yahoo.com.br>
2022-04-14 10:01:34 -0300

    interceptador indicador de trabalho processando

--- src/app/shared/components/loading/loading.module.ts
+++ src/app/shared/components/loading/loading.module.ts
@@ -1,10 +1,17 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CommonModule } from "@angular/common";
-import {NgModule} from "@angular/core";
+import { NgModule } from "@angular/core";
 import { LoadingComponent } from "./loading.component";
+import { LoadingInterceptor } from './loading.interceptor';
 
 @NgModule({
     declarations: [LoadingComponent],
     exports: [LoadingComponent],
-    imports: [CommonModule]
+    imports: [CommonModule],
+    providers: [{
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true
+    }]
 })
 export class LoadingModule { }
