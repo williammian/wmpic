79afdb91e76bce196d2d593da7fbda4598520a3a
William Mian <william_mian@yahoo.com.br>
2022-04-14 11:06:05 -0300

    componente menu

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: 'ap-menu',
+    templateUrl: './menu.component.html'
+})
+export class MenuComponent {
+
+  isShown = false;
+
+  toggle() {
+      this.isShown = !this.isShown;
+  }
+
+}
