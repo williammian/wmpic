d3d8ccc8ff85365324fafd037fe9d1f68dcf8812
William Mian <william_mian@yahoo.com.br>
2022-04-18 12:55:36 -0300

    validando ao deixar o campo

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -65,13 +65,15 @@ export class SignUpComponent implements OnInit {
   }
 
   signup() {
-    const newUser = this.signupForm.getRawValue() as NewUser;
-    this.signUpService
-        .signup(newUser)
-        .subscribe(
-            () => this.router.navigate(['']),
-            err => console.log(err)
-        );
+    if(this.signupForm.valid && !this.signupForm.pending) {
+      const newUser = this.signupForm.getRawValue() as NewUser;
+      this.signUpService
+          .signup(newUser)
+          .subscribe(
+              () => this.router.navigate(['']),
+              err => console.log(err)
+          );
+    }
   }
 
 }
