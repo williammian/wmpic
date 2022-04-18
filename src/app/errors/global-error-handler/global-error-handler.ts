fe9d9604b85e21bbbeaea0e3790233df734f40bd
William Mian <william_mian@yahoo.com.br>
2022-04-18 10:05:35 -0300

    exibindo mais informacoes do erro

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,17 +1,39 @@
-import { ErrorHandler } from "@angular/core";
+import { ErrorHandler, Injectable, Injector } from "@angular/core";
 import * as StackTrace from "stacktrace-js";
+import { LocationStrategy, PathLocationStrategy } from "@angular/common";
 
+import { UserService } from "src/app/core/user/user.service";
+
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
-    handleError(error: any): void {
-      console.log('passei pelo handler')
-      StackTrace
-          .fromError(error)
-          .then(stackFrames => {
-            const stackAsString = stackFrames
-            .map(sf => sf.toString())
-            .join('\n')
-            console.log(stackAsString);
-          });
-    }
+  constructor(private injector: Injector) {}
+
+  handleError(error: any): void {
+    console.log('passei pelo handler');
+
+    const location = this.injector.get(LocationStrategy);
+    const userService = this.injector.get(UserService);
+
+    const url = location instanceof PathLocationStrategy
+            ? location.path()
+            : '';
+
+    const message = error.message
+          ? error.message :
+          error.toString();
+
+    StackTrace
+        .fromError(error)
+        .then(stackFrames => {
+          const stackAsString = stackFrames
+          .map(sf => sf.toString())
+          .join('\n')
+
+          console.log(message);
+          console.log(stackAsString);
+          console.log('o que será enviado para o servidor')
+          console.log({ message, url, userName: userService.getUserName(), stack: stackAsString});
+        });
+  }
 }
