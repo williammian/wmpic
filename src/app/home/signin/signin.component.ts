fd00f736381756ef1c7c41f5cb674a5ea9000fa1
William Mian <william_mian@yahoo.com.br>
2022-04-18 15:38:40 -0300

    obtendo QueryParams

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,7 @@
 import { PlatformDetectorService } from './../../core/platform-detector/platform-detector.service';
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { AuthService } from "src/app/core/auth/auth.service";
 
 @Component({
@@ -9,6 +9,7 @@ import { AuthService } from "src/app/core/auth/auth.service";
 })
 export class SignInComponent implements OnInit {
 
+  fromUrl: string;
   loginForm: FormGroup;
   @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
@@ -16,10 +17,15 @@ export class SignInComponent implements OnInit {
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private platformDetectorService: PlatformDetectorService
+    private platformDetectorService: PlatformDetectorService,
+    private activatedRoute: ActivatedRoute
     ) { }
 
   ngOnInit(): void {
+    this.activatedRoute
+        .queryParams
+        .subscribe(params => this.fromUrl = params['fromUrl']);
+
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
@@ -38,15 +44,16 @@ export class SignInComponent implements OnInit {
     this.authService
         .authenticate(userName, password)
         .subscribe(
-          () => this.router.navigate(['user', userName]),
+          () => this.fromUrl
+              ? this.router.navigateByUrl(this.fromUrl)
+              : this.router.navigate(['user', userName])
+          ,
           err => {
-            console.log(err);
-            this.loginForm.reset();
-
-            this.platformDetectorService.isPlatformBrowser() &&
-              this.userNameInput.nativeElement.focus();
-
-            alert('Invalid user name or password');
+              console.log(err);
+              this.loginForm.reset();
+              this.platformDetectorService.isPlatformBrowser() &&
+                  this.userNameInput.nativeElement.focus();
+              alert('Invalid user name or password');
           }
         );
 
